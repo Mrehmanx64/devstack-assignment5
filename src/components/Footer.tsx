bee7dbc173cead365eab228a635503fd6d6ac3cb
Munna Rehman <rehmanroxxs@gmail.com>
2026-09-14 16:15:25 +0600

    center footer content on mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="mt-16 mb-12 pt-16 border border-white border-t-[#F1F5F9]">
       <div className="container mx-auto px-4">
         <div className="mb-14 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-10">
-          <div className="md:col-span-2 flex flex-col gap-3">
+          <div className="md:col-span-2 flex flex-col gap-3 text-center md:text-left items-center md:items-start">
             <img src={LogoText} alt="Dev Stack" className="w-30" />
             <p className="max-w-md text-xs leading-6 text-[#64748B]">
               Curated tools, technologies, and resources for developers building
